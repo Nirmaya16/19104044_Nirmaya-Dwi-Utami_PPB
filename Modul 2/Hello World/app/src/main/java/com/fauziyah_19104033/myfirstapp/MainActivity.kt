8d36b4ea0f5d952acc1ac62b3c7398fdb84c858e
Nirmaya Dwi Utami <19104044.ittelkom-pwt.ac.id@gmail.com>
2022-02-08 06:33:28 +0700

    Praktikum pertemuan2

---
+++ Modul 2/Hello World/app/src/main/java/com/fauziyah_19104033/myfirstapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nirmaya_19104044.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
